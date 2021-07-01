74fc29bbb1cf720ea2f5c1c22070d9c444b455fc
Edwin-1012 <bttagaredwin@gmail.com>
2021-06-30 20:05:17 -0500

    Subiendo el archivo de access y index

---
+++ CafeHuertoElMamey/index.php
@@ -0,0 +1,11 @@
+<?php
+/* Inicio carga las clases iniciales */
+define("LLAVE","sistemas6us");
+require_once 'libs/database.php';
+require_once 'libs/Controlador.php';
+require_once 'libs/Control.php';
+require_once 'libs/Sesion.php';
+require_once 'libs/Valida.php';
+require_once 'config/config.php';
+$control = new Control();
+?>
