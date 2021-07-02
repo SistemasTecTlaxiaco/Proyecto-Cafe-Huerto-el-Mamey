04d07fec02eaeab81e2a98a95fb559b8e85a8bf0
Edwin-1012 <bttagaredwin@gmail.com>
2021-07-02 01:22:24 -0500

    Revisando el config

---
+++ CafeHuertoElMamey/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('URL','https://localhost/Cafe/');
+define('HOST', 'localhost:3307');
+define('DB', 'bdcafetienda');
+define('USER', 'root');
+define('PASSWORD', 'jsohsjhdhjks');
+define('CHARSET', 'utf8');
+?>
