ab4e460e56fb0dc49fc4620c963422cc6049d19f
Edwin Bautista García <72049201+Edwin-1012@users.noreply.github.com>
2021-10-12 20:51:46 -0500

    Update buscarVista
    Se cambió el nombre de las variables

--- CafeHuertoElMamey/views/buscarVista.php
+++ CafeHuertoElMamey/views/buscarVista.php
@@ -3,17 +3,17 @@
         <div class="card p-4 bg-light">
           <?php
             $ren = 0;
-            for ($i=0; $i < count($datos["data"]); $i++) { 
+            for ($i=0; $i < count($Datos["data"]); $i++) { 
               if ($ren==0) {
                 print "<div class='row'>";
               }
               print "<div class='card pt-2 col-sm-3'>";
-              print "<img src='".URL."public/img/".$datos['data'][$i]["imagen"]."' ";
+              print "<img src='".URL."public/img/".$Datos['data'][$i]["imagen"]."' ";
               print "class='img-responsive' style='width:100%; height:140px;' ";
-              print "alt='".$datos['data'][$i]["nombre"]."'/>";
+              print "alt='".$Datos['data'][$i]["nombre"]."'/>";
               print "<p><a href='".URL."admonProductos/producto/";
-              print $datos['data'][$i]["id"]."'>";
-              print $datos['data'][$i]["nombre"]."</a></p>";
+              print $Datos['data'][$i]["id"]."'>";
+              print $Datos['data'][$i]["nombre"]."</a></p>";
               print "</div>";
               $ren++;
               if ($ren==4) {
