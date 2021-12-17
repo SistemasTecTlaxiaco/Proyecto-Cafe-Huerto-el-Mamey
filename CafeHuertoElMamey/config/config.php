a05fa96159e65139882067c0dada08bec09c9dfe
Alexis <72049151+10154@users.noreply.github.com>
2021-12-17 01:07:56 -0600

    cambiar la clave

--- CafeHuertoElMamey/config/config.php
+++ CafeHuertoElMamey/config/config.php
@@ -3,6 +3,6 @@ define('URL','https://localhost/Cafe/');
 define('HOST', 'localhost');//Modificando el host
 define('DB', 'bdcafetienda');
 define('USER', 'root');
-define('PASSWORD', '');
+define('PASSWORD', 'jsohsjhdhjks');
 define('CHARSET', 'utf8');
 ?>
