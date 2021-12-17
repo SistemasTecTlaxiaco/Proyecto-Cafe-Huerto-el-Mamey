b827f118a699f799227187e1f756d8e316a6b2e3
Alexis <72049151+10154@users.noreply.github.com>
2021-12-17 01:05:00 -0600

    Se deja son contraseña para que los usuarios no tengan acceso a a la base de datos

--- CafeHuertoElMamey/config/config.php
+++ CafeHuertoElMamey/config/config.php
@@ -3,6 +3,6 @@ define('URL','https://localhost/Cafe/');
 define('HOST', 'localhost');//Modificando el host
 define('DB', 'bdcafetienda');
 define('USER', 'root');
-define('PASSWORD', 'jsohsjhdhjks');
+define('PASSWORD', '');
 define('CHARSET', 'utf8');
 ?>
