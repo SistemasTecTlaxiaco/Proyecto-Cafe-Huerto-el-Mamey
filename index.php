cbf2504b586e497cc2cacb7a2975bc3beeb79fa8
Ivan Montoya <moty5871@gmail.com>
2022-06-12 21:38:02 -0500

    Removiendo Clases

--- index.php
+++ index.php
@@ -7,6 +7,5 @@ require_once 'libs/Control.php';
 require_once 'libs/Sesion.php';
 require_once 'libs/Valida.php';
 require_once 'config/config.php';
-require_once("index.html");
 $control = new Control();
 ?>
